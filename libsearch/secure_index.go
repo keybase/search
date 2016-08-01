ca19cc7ce374a108b94f2a2c348a04706133d96a
Jiaxin Guan <jxguan@stanford.edu>
2016-08-01 16:59:06 -0700

    Refactor library code into libsearch

---
+++ libsearch/secure_index.go
@@ -0,0 +1,70 @@
+package libsearch
+
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/binary"
+	"errors"
+	"hash"
+
+	"github.com/jxguan/go-datastructures/bitarray"
+)
+
+// SecureIndex defines the elements in a secure index.
+type SecureIndex struct {
+	BloomFilter bitarray.BitArray // The blinded bloom filter, which is the main part of the index.
+	DocID       int               // The document ID that this index is for.
+	Size        uint64            // The number of buckets in the bloom filter.
+	Hash        func() hash.Hash  // The hash function to be used for HMAC.
+}
+
+// MarshalBinary implements the encoding.BinaryMarshaler interface.
+func (si *SecureIndex) MarshalBinary() ([]byte, error) {
+	bfBytes, err := bitarray.Marshal(si.BloomFilter)
+	if err != nil {
+		return nil, err
+	}
+	length := 24 + len(bfBytes)
+	result := make([]byte, length)
+	binary.PutVarint(result, int64(si.DocID))
+	binary.PutVarint(result[8:], int64(si.Hash().Size()))
+	binary.PutUvarint(result[16:], si.Size)
+	copy(result[24:], bfBytes)
+	return result, nil
+}
+
+// Reads an int from the input byte slice.
+func readInt(input []byte) (int, error) {
+	num, numBytes := binary.Varint(input)
+	if numBytes <= 0 {
+		return 0, errors.New("cannot read the int")
+	}
+	return int(num), nil
+}
+
+// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface.
+func (si *SecureIndex) UnmarshalBinary(input []byte) error {
+	if len(input) < 24 {
+		return errors.New("insufficient binary length")
+	}
+	var err error
+	si.DocID, err = readInt(input[0:8])
+	if err != nil {
+		return err
+	}
+	var hashLen int
+	hashLen, err = readInt(input[8:16])
+	if err != nil {
+		return err
+	} else if hashLen == 256/8 {
+		si.Hash = sha256.New
+	} else if hashLen == 512/8 {
+		si.Hash = sha512.New
+	}
+	si.Size, _ = binary.Uvarint(input[16:24])
+	si.BloomFilter, err = bitarray.Unmarshal(input[24:])
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ libsearch/secure_index_test.go
@@ -0,0 +1,44 @@
+package libsearch
+
+import (
+	"crypto/sha256"
+	"search/prototype/util"
+	"testing"
+
+	"github.com/jxguan/go-datastructures/bitarray"
+)
+
+// TestMarshalAndUnmarshal tests the `Marshal` and `Unmarshal` functions.
+// Checks that after a pair of `Marshal` and `Unmarshal` operations, the orignal
+// SecureIndex is correctly reconstructed from the byte slice.
+func TestMarshalAndUnmarshal(t *testing.T) {
+	si := new(SecureIndex)
+	si.BloomFilter = bitarray.NewSparseBitArray()
+	for i := 0; i < 1000; i++ {
+		si.BloomFilter.SetBit(util.RandUint64n(1000000))
+	}
+	si.DocID = 42
+	si.Size = uint64(1900000)
+	si.Hash = sha256.New
+	bytes, err1 := si.MarshalBinary()
+	if err1 != nil {
+		t.Fatalf("Error when marshaling the index")
+	}
+	si2 := new(SecureIndex)
+	err2 := si2.UnmarshalBinary(bytes)
+	if err2 != nil {
+		t.Fatalf("Error when unmarshaling the index")
+	}
+	if si2.DocID != si.DocID {
+		t.Fatalf("DocID does not match")
+	}
+	if si2.Hash().Size() != si.Hash().Size() {
+		t.Fatalf("Hash does not match")
+	}
+	if si2.Size != si.Size {
+		t.Fatalf("Size does not match")
+	}
+	if !si2.BloomFilter.Equals(si.BloomFilter) {
+		t.Fatalf("BloomFilter does not mtach")
+	}
+}
