139eba0074b312863245c0a33c9c0c8513e5357a
Jiaxin Guan <jxguan@stanford.edu>
2016-08-26 16:58:01 -0700

    Add comments

--- client/client/main.go
+++ client/client/main.go
@@ -17,6 +17,8 @@ var clientDirectory = flag.String("client_dir", "/keybase/private/jiaxin", "the 
 var port = flag.Int("port", 8022, "the port that the search server is listening on")
 var ipAddr = flag.String("ip_addr", "127.0.0.1", "the IP address that the search server is listening on")
 
+// periodicAdd scans the files in the client directory every minute and adds
+// their indexes to the search server.
 func periodicAdd(cli *client.Client) {
 	for {
 		files, _ := ioutil.ReadDir(*clientDirectory)
@@ -34,18 +36,20 @@ func main() {
 
 	cli, err := client.CreateClient(context.TODO(), *ipAddr, *port, []byte("A really really really long Master Secret, oh god it's so long"), *clientDirectory)
 	if err != nil {
-		fmt.Println("Cannot initiate the client: %s", err)
+		fmt.Printf("Cannot initiate the client: %s\n", err)
 		os.Exit(1)
 	}
 	go periodicAdd(cli)
 
 	reader := bufio.NewReader(os.Stdin)
 	for {
+		fmt.Print("Please enter word to search for: ")
 		keyword, _ := reader.ReadString('\n')
 		keyword = keyword[:len(keyword)-1]
 		files, err := cli.SearchWord(keyword)
 		if err != nil {
-			fmt.Println("oops, error!")
+			fmt.Printf("Error when searching word %s: %s", keyword, err)
+			continue
 		}
 		fmt.Println(files)
 	}
